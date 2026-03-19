082c25d00dae922fa2544898e5eccc090db5f8bb
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-19 19:21:43 +0530

    terraform

--- main.tf
+++ main.tf
@@ -165,12 +165,12 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 resource "aws_route_table_association" "private" {
-  count = length(public_subnet_cidrs)
+  count = length(private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 resource "aws_route_table_association" "database" {
-  count = length(public_subnet_cidrs)
+  count = length(database_subnet_cidrs)
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
